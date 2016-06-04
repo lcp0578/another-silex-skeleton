741ff8cd17720efd7f02120241f4283ac8d6cecd
李春鹏 <lcp0578@gmail.com>
2016-06-04 15:17:26 +0800

    [MOD]modify error message

--- src/controller/errorHandlers.php
+++ src/controller/errorHandlers.php
@@ -5,5 +5,5 @@ use Symfony\Component\HttpFoundation\Response;
  * @var $app \Silex\Application
  */
 $app->error(function(\Exception $e, $code){
-    return new Response($e->getMessage() . $e->getCode());
+    return new Response('msg:' . $e->getMessage() . '; code=' .$e->getCode());
 });
