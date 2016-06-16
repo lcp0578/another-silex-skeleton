1fd09e96ae54fa88c89451136697f64f8d439283
李春鹏 <lcp0578@gmail.com>
2016-06-16 16:15:48 +0800

    [ADD] 增加了配置项的文件

---
+++ src/parameters/CoreParameters.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * CoreParameters
+  *
+  * @package: parameters
+  * @author: lcp0578@gmail.com
+  * @date: 2016-06-14 AM12:20:12
+  * @version: 0.0.1
+  * @copyright: http://lcpeng.cn
+  */
+$app['request.http_port'] = 8080; // default 80
