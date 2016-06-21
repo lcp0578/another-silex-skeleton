599b607a46d926a534e3910e64fb1821dd585e8e
李春鹏 <lcp0578@gmail.com>
2016-06-21 23:34:00 +0800

    [ADD] Hello controller provider

---
+++ src/controller/Lcp/HelloControllerProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+  * filename
+  *
+  * @package: packname
+  * @author: lcp0578@gmail.com
+  * @date: 2016-06-21 11:29:03
+  * @version: 0.0.1
+  * @copyright: http://lcpeng.cn
+  */
+namespace Lcp;
+
+use Silex\Api\ControllerProviderInterface;
+use Silex\Application;
+
+class helloControllerProvider implements ControllerProviderInterface
+{
+    public function connect(Application $app)
+    {
+        
+    }
+}
