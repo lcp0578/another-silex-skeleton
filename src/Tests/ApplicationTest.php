27dd4f2862d8b3ede66dd83a609a0814470fbb2d
李春鹏 <lcp0578@gmail.com>
2016-06-23 23:55:53 +0800

    [ADD] add application test

---
+++ src/Tests/ApplicationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+  * filename
+  *
+  * @package: packname
+  * @author: lcp0578@gmail.com
+  * @date: 2016年6月23日 下午11:53:39
+  * @version: 0.0.1
+  * @copyright: http://lcpeng.cn
+  */
+use Silex\WebTestCase;
+
+class ApplicationTest extends WebTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+    }
+    
+    public function createApplication()
+    {
+        
+    }
+}
