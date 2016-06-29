af0bc1a7ec80d6e41d99403a4f2dc6cf30cd2adc
李春鹏 <lcp0578@gmail.com>
2016-06-30 00:06:46 +0800

    [MOD] controller implementation

--- src/controller/JsonController.php
+++ src/controller/JsonController.php
@@ -10,8 +10,21 @@
   * @copyright: http://lcpeng.cn
   */
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 $app->before(function(Request $request){
     if(0 === strpos($request->headers->get('Content-Type'), 'application/json')){
-        
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
     }
+});
+
+$app->post('/blog/posts', function(Request $request) use ($app){
+    $post = array(
+        'title' => $request->request->get('title'),
+        'body' => $request->request->get('body')
+    );
+    
+    $post['id'] = createPost($data);
+    return $app->json($data, 201);
 });
